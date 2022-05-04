fb511d721efacbebfce443edf473af7eaa533525
Rittik Banerjee <rittikbanerjee7@gmail.com>
2022-05-05 02:53:13 +0530

    added Beggars Outside Temple

--- Arrays/Max Non Negative SubArray.java
+++ Arrays/Max Non Negative SubArray.java
@@ -1,4 +1,5 @@
 /*
+Problem Description
 Given an array of integers, A of length N, find out the maximum sum sub-array of non negative numbers from A.
 
 The sub-array should be contiguous i.e., a sub-array created by choosing the second and fourth element and skipping the third element is invalid.
